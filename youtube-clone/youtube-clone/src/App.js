fc5ee94bd17ebad1b01d27f21c01c08cb0721788
ayd823 <ayd823@gmail.com>
2022-11-20 16:14:45 -0500

    fixed nav error

--- youtube-clone/youtube-clone/src/App.js
+++ youtube-clone/youtube-clone/src/App.js
@@ -11,13 +11,13 @@ import Videos from "./components/Videos";
 import "./App.css";
 
 function App() {
-  fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+  // fetch(
+  //   `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
 
   return (
     <div className="wrapper">
