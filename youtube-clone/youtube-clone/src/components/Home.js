2aa5cb2848c7423450f10b9c6aac018911091f53
ayd823 <ayd823@gmail.com>
2022-11-20 17:18:34 -0500

    localStorage/search function

--- youtube-clone/youtube-clone/src/components/Home.js
+++ youtube-clone/youtube-clone/src/components/Home.js
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
 export default function Home() {
-  return;
+  const [search, setSearch] = useState("");
+  const [videos, setVideos] = useState([]);
+
+  function resetSearch() {
+    setSearch("");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const url = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${search}`;
+
+    console.log(url);
+
+    const result = JSON.parse(window.localStorage.getItem(search));
+
+    if (result) {
+      setVideos(result);
+    } else {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((res) => {
+          console.log(`Fetch worked!`);
+
+          window.localStorage.setItem(search, JSON.stringify(res));
+
+          setVideos(res);
+        });
+    }
+    resetSearch();
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Search"></label>
+        <input
+          value={search}
+          type="text"
+          id="search"
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <button>Search</button>
+      </form>
+    </div>
+  );
 }
